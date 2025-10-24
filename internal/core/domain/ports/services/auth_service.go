f0bb09170deb59d153cde1e619ca0fa82cdfc08b
suphanatchanlek <suphanatchanlek@gmail.com>
2025-10-24 11:18:36 +0700

    Add Port/services For authservice.go

---
+++ internal/core/domain/ports/services/auth_service.go
@@ -0,0 +1,18 @@
+// internal/core/domain/ports/services/auth_service.go
+
+package services
+
+import (
+	"github.com/suphanatchanlek30/fiber-commerce-api/internal/core/domain/entities"
+)
+
+type AuthService interface {
+	// สร้างผู้ใช้ใหม่
+	Register(req entities.RegisterRequest) (*entities.User, error)
+	// ผู้ใช้เข้าสู่ระบบ
+	Login(req entities.LoginRequest) (*entities.LoginResponse, error)
+	// ดึงข้อมูลผู้ใช้ตาม ID
+	GetUserByID(id uint) (*entities.User, error)
+	// อัปเดตข้อมูลผู้ใช้
+	UpdateUser(user *entities.User) error
+}
