a8c28399a3f256ff92214d995baecae2a7d23339
suphanatchanlek <suphanatchanlek@gmail.com>
2025-11-03 14:41:19 +0700

    Add Logic in floder internal/core/services/

--- pkg/utils/validator.go
+++ pkg/utils/validator.go
@@ -8,7 +8,7 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
-// สร้างตัวแปร validate สำหรับการตรวจสอบความถูกต้องของข้อมูล เรียกใช้ validator.New()
+// สร้างตัวแปร validate สำหรับการตรวจสอบความถูกต้องของข้อมูล
 var validate = validator.New()
 
 // สร้างฟังก์ชัน Validate ที่ใช้สำหรับตรวจสอบความถูกต้องของข้อมูล
